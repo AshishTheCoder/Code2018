5d56d37aedad62153bc5deee7e80220e2bdd22f0
AshishTheCoder <37212413+AshishTheCoder@users.noreply.github.com>
2018-03-09 17:12:38 +0530

    Collection.js

---
+++ seciportal/lib/js/Collections/collections.js
@@ -0,0 +1,41 @@
+//Reg=new Mongo.Collection('register');
+//Continent=new Mongo.Collection('Continent');
+//Country=new Mongo.Collection('Country');
+//State=new Mongo.Collection('State');
+//City=new Mongo.Collection('City');
+//Registration=new Mongo.Collection('registration');
+Schedule=new Mongo.Collection('schedule');
+Student=new Mongo.Collection('Student');
+Marks=new Mongo.Collection('Marks');
+SPD=new Mongo.Collection('SPD');
+DISCOM=new Mongo.Collection('DISCOM');
+Tickets=new Mongo.Collection('Tickets');
+// this.Images = new FilesCollection({collectionName: 'Images'});
+
+// Excelfiles=new FS.Collection("Excelfiles",{
+//   stores:[new FS.Store.GridFS("Excelfiles")]
+// });
+//
+// Excelfiles.allow({
+//   insert:function (doc) {
+//     return true;
+//   },
+//   update:function (doc,fields,modifier) {
+//     return true;
+//   },
+//   download:function () {
+//     return true;
+//   }
+// });
+//
+// Userfiles=new Mongo.Collection("Userfiles");
+//
+// Userfiles.allow({
+//   insert:function (doc) {
+//     return true;
+//   },
+//   update:function (doc,fields,modifier) {
+//     return true;
+//   }
+//
+// });
