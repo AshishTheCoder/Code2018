7556a231af2072f4d08f88159445facbdfc139e3
AshishTheCoder <37212413+AshishTheCoder@users.noreply.github.com>
2018-03-09 17:12:54 +0530

    route.js

---
+++ route.js
@@ -0,0 +1,55 @@
+Router.route('/', function () {
+  this.render('registers');
+});
+
+Router.route('/login', function () {
+  this.render('logins');
+});
+Router.route('/dashboard', function () {
+  this.render('dashboards');
+});
+Router.route('/city', function () {
+  this.render('city');
+});
+Router.route('/continent', function () {
+  this.render('continent');
+});
+Router.route('/country', function () {
+  this.render('country');
+});
+Router.route('/output', function () {
+  this.render('output');
+});
+Router.route('/register', function () {
+  this.render('register');
+});
+Router.route('/state', function () {
+  this.render('state');
+});
+Router.route('/uploadForm', function () {
+  this.render('uploadForm1');
+});
+Router.route('/report', function () {
+  this.render('report');
+});
+Router.route('/user1', function () {
+  this.render('user1');
+});
+Router.route('/viewschedule', function () {
+  this.render('viewschedule');
+});
+Router.route('/emails', function () {
+  this.render('email');
+});
+Router.route('/entry', function () {
+  this.render('entrymodule');
+});
+Router.route('/userprofile', function () {
+  this.render('userprofile');
+});
+Router.route('/tickets', function () {
+  this.render('tickets');
+});
+Router.route('/viewtickets', function () {
+  this.render('viewtickets');
+});
